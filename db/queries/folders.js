5a01efc8cc0dc327f8452c152218de5271e7c782
Billy Lachenal <billylachenal@gmail.com>
2026-04-14 13:40:08 -0500

    Database tests now passing

--- db/queries/folders.js
+++ db/queries/folders.js
@@ -1,10 +1,10 @@
 import db from "#db/client";
 
-export async function createFolder(name){
+export async function createFolder({name}){
     const sql = `
         INSERT INTO folders (name) 
         VALUES ($1) RETURNING * 
         `;
-        const {row: folders} = await db.query(sql, [name]);
+        const {rows: folders} = await db.query(sql, [name]);
         return folders[0];
 }
--- db/seed.js
+++ db/seed.js
@@ -1,5 +1,8 @@
 import db from "#db/client";
+
 import { faker } from "@faker-js/faker";
+import { createFolder } from "./queries/folders.js";
+import { createFile } from "./queries/files.js";
 
 await db.connect();
 await seed();
@@ -8,7 +11,17 @@ console.log("🌱 Database seeded.");
 
 async function seed() {
   // TODO
+  
   for (let i = 0; i < 3; i++){
-
+    const folder = await createFolder({
+      name: faker.database.engine()
+    });
+      for(let j=0; j<5; j++){
+        await createFile({
+        name: faker.system.commonFileName(),
+        size: faker.number.int({max: 1200}),
+        folderId:folder.id
+      });
+    } 
   }
 }
